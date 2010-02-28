af2917e8a40214e4aa70fe8bea6fba146c90c028
Nicholas C. Zakas <nicholas.c.zakas@nczonline.net>
2010-02-28 11:32:39 -0800

    Updated build system

--- build/parser-lib.js
+++ build/parser-lib.js
@@ -1,3 +1,25 @@
+/* 
+Copyright (c) 2009 Nicholas C. Zakas. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
 
 /**
  * A generic base to inherit from for any object
@@ -82,7 +104,8 @@ EventTarget.prototype = {
             listeners.splice(i, 1);
         }            
     }
-};/**
+};
+/**
  * Convenient way to read through strings.
  * @class StringReader
  * @constructor
@@ -341,7 +364,8 @@ StringReader.prototype = {
         }
     }
 
-};/**
+};
+/**
  * Generic TokenStream providing base functionality.
  * @class TokenStream
  * @constructor
@@ -711,4 +735,5 @@ TokenStream.prototype = {
     }
 
 };
+
 

3f4fba4bccb6026c37c96b8eba0d154ceadbd72b
C. Scott Ananian <cscott@cscott.net>
2016-02-01 17:42:23 -0500

    Strictly parse escape sequences in identifiers.
    Make our escape sequence parsing match
    https://www.w3.org/TR/CSS21/grammar.html#scanner
    
    In particular, don't accept "\\\r" as a valid escape sequence, and
    swallow trailing space for every hex escape (not just 2-digit ones).
    
    We also tweak `readName` in the tokenizer to unescape the names it
    reads, which makes `@ch\041 rset` correctly parse as a `CHARSET_SYM`.

--- tests/css/TokenStream.js
+++ tests/css/TokenStream.js
@@ -138,7 +138,9 @@
             "#h\\0fllo"         : [CSSTokens.HASH],
             "#ffeeff"           : [CSSTokens.HASH],
             "#\\31 a2b3c"        : [CSSTokens.HASH],
-            "#r0\\.5"            : [CSSTokens.HASH]
+            "#r0\\.5"            : [CSSTokens.HASH],
+            // Invalid escape sequence
+            "#a\\\r"             : [CSSTokens.HASH, CSSTokens.CHAR, CSSTokens.S]
         }
     }));
 
@@ -150,6 +152,7 @@
 
         var atRules = {
             "@charset"      : CSSTokens.CHARSET_SYM,
+            "@ch\\041 rset" : CSSTokens.CHARSET_SYM,
             "@import"       : CSSTokens.IMPORT_SYM,
             "@page"         : CSSTokens.PAGE_SYM,
             "@media"        : CSSTokens.MEDIA_SYM,
